cd53e0d87a5f90e93d187a56029068771960c819
Clarence <thomasravelg@gmail.com>
2018-09-08 10:13:20 -0400

    Add character control left/right/jump

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+    public float speed = 5.0f;
+    Rigidbody2D rb;
+    public LayerMask groundLayers;
+
+    public float jumpForce = 7;
+    public CircleCollider2D col;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<CircleCollider2D>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        var movement = new Vector2(moveHorizontal, 0);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public float speed = 5.0f;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+    }
+}
